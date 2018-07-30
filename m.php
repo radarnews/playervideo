086bfcb2bd8761a4addf7e2b0afbe68c7166da95
radarnews <programa@rederadarnews.info>
2018-07-30 17:50:40 -0300

    Delete m.php

--- m.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once('mega.class.php');
-$megafile = new MEGA('http://mega.co.nz/#'.$_GET['id']);
-//$megafile->download();
-// OR
-$megafile->stream_download(); // to download using streams
-?>
